0f41219041c00890b303f1a250d0731f88bd2c4a
Adam Biegert <118407273+AdamaStarkN7@users.noreply.github.com>
2025-02-08 22:43:36 -0700

    Add files via upload

---
+++ html/config.js
@@ -0,0 +1,79 @@
+/*=================================================================*\
+/* By: 			|	Nevera Development  							|
+/* FiveM: 		|	https://forum.cfx.re/u/neveradevelopment		|
+/* Discord: 	|	https://discord.gg/NeveraDev/tw28AqrgWU  		|
+/*=================================================================*/
+/* If you have any problems you can contact us via discord. <3     */
+
+
+var staff_team = [
+
+	{
+		"name":"AdamaStark_N7",
+		"image":"https://r2.fivemanage.com/D2LH4qugIBTzKp6BdG8Co/images/Loading_Screen.png",
+		"rank":" Server Owner"
+	},
+	{
+		"name":"AdamaStark_N7",
+		"image":"https://r2.fivemanage.com/D2LH4qugIBTzKp6BdG8Co/images/Loading_Screen.png",
+		"rank":"Lead Server Developer"
+	},
+	{
+		"name":"Cattt",
+		"image":"https://r2.fivemanage.com/D2LH4qugIBTzKp6BdG8Co/8-zmcM4d6g0yctBfw.png",
+		"rank":"Lead Server Admin"
+	},
+	{
+		"name":"Sgt_Nathanial",
+		"image":"https://r2.fivemanage.com/D2LH4qugIBTzKp6BdG8Co/images/Loading_Screen.jpg",
+		"rank":"Lead Server Moderator",
+	},
+	{
+		"name":"Monkey595",
+		"image":"https://r2.fivemanage.com/D2LH4qugIBTzKp6BdG8Co/images/Loading_Screen.jpg",
+		"rank":"Lead Server Tester",
+	},
+]
+
+// Staff Settings
+const showStaffTeam = true
+const showPlayersList = true
+
+// orange
+// red
+// blue
+// green
+// pink
+// purple
+const theme = "blue"
+
+// Text settings
+const name = "<strong>PARA</strong>GON"
+const underName = "ROLE<b>PLAY</b><strong> 3.5</strong>"
+const desc = "A unique FiveM Roleplay server featuring an extensive collection of custom scripts, maps, vehicles, unique weapons, and much more!"
+
+// Social media
+const discord = "https://discord.gg/Cmakb5udQg"
+const instagram = "" // If = "" then icon will not show up on screen
+const youtube = ""
+const twitter = ""
+
+// Player List
+const serverCode = "av7v49" //Your server CODE from fivem. (cfx.re/join/abcdef) this ABCDEF is your code, paste it in serverCode.
+const playerProfileImage = "https://r2.fivemanage.com/D2LH4qugIBTzKp6BdG8Co/images/Loading_Screen.jpg"
+
+// Video Settings
+const videoBlur = 0
+var videoOpacity = 0.3
+
+// const youtubeVideo = "https://www.youtube.com/watch?v=Q_5z4n5TZ2Q" // Sleepwalking
+// const youtubeVideo = "https://www.youtube.com/watch?v=KrM107U_uRg" // Street Lights
+// const youtubeVideo = "https://www.youtube.com/watch?v=UZ7y3yuqgRg" // Running Wild Remix
+const youtubeVideo = "https://www.youtube.com/watch?v=SztA0gUsvOw" // Running Wild
+const showVideo = false
+
+// if localAudio is true, then loading will load "audio.mp3" file and play it except youtube audio.
+// if localAudio is false, then loading will load youtube audio.
+const localAudio = false // true or false
+
+// FOR CUSTOM AUDIO, REPLACE FILE "audio.mp3" WITH YOUR .mp3 FILE.
